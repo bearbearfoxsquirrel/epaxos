6a26aee222f1a1cdebbdb0567a624c626df503e7
SirMiguel <michaeldavis96@zoho.com>
2021-08-13 11:57:56 +0100

    adjusted ms skip for mencius

--- src/mencius/mencius.go
+++ src/mencius/mencius.go
@@ -14,7 +14,7 @@ import (
 )
 
 const CHAN_BUFFER_SIZE = 200000
-const WAIT_BEFORE_SKIP_MS = 50
+const WAIT_BEFORE_SKIP_MS = 250
 const NB_INST_TO_SKIP = 100000
 const MAX_SKIPS_WAITING = 20
 const TRUE = uint8(1)
